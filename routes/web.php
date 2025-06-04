cab91c89a0a5f48b887d373ca99b91ba80d3df33
nyx08dreamer <nikirv0808@gmail.com>
2025-06-04 09:37:21 -0400

    test de index y login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('holamundo');
+    return view('template');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
 });
