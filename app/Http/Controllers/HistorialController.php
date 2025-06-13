53293c50e50fe3f3232599d56d5990033dd00d2d
ByDead75 <stefanoparra15@gmail.com>
2025-06-13 08:44:31 -0400

    Cambios del miércoles

--- app/Http/Controllers/HistorialController.php
+++ app/Http/Controllers/HistorialController.php
@@ -17,10 +17,17 @@ class HistorialController extends Controller
 
     public function registros_historial (Request $request) {
 
+<<<<<<< Updated upstream
         if ($request->ajax()) {
             
             $solicitudes_model = new Solicitudes;
             $solicitudes = $solicitudes_model->get_solicitudes();
+=======
+
+        //Este comando indica la cantidad de paginación que se le asigna a la tabla
+
+        $solicitudes = Solicitudes::paginate(25);
+>>>>>>> Stashed changes
 
             $datatables = DataTables::of($solicitudes)
                 ->addIndexColumn()
